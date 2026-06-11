6c344c3217ecf73db5d137483c50698151bf2dec
Angélica Anjos <angellica.anjos@hotmail.com>
2026-06-11 18:08:28 -0300

    perf: otimiza carregamento das imagens dos destinos

--- src/components/ui/DestinoCard.jsx
+++ src/components/ui/DestinoCard.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from "react-icons/fa6";
 function DestinoCard({ pais, preco, imagem }) {
   return (
     <div className="bg-white border border-gray-300 rounded-2xl transition-all duration-300 hover:-translate-y-2 hover:shadow-xl">
-      <img className="rounded-t-2xl h-full" src={imagem} alt="toronto" />
+      <img className="rounded-t-2xl h-full" src={imagem} loading="lazy" alt={pais} />
 
       <div className="py-3 px-5">
         <h3 className="text-xl font-medium">{pais}</h3>
