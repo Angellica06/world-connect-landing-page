3727be17c60a3bc360cd8d122bd12afdf26b339b
Angélica Anjos <angellica.anjos@hotmail.com>
2026-06-11 17:59:12 -0300

    feat: adiciona imagens e conteúdo dinâmico aos cards de destinos

--- src/components/sections/Destinos.jsx
+++ src/components/sections/Destinos.jsx
@@ -1,27 +1,74 @@
 import DestinoCard from "../ui/DestinoCard";
+import Imagem1 from "../../assets/imagens/canada.jpg";
+import Imagem2 from "../../assets/imagens/irlanda.jpg";
+import Imagem3 from "../../assets/imagens/australia.jpg";
+import Imagem4 from "../../assets/imagens/estadosunidos.jpg";
+import Imagem5 from "../../assets/imagens/reinounido.png";
+
+const destinos = [
+  {
+    id: 1,
+    pais: "Canadá",
+    preco: "8.900",
+    imagem: Imagem1,
+  },
+  {
+    id: 2,
+    pais: "Irlanda",
+    preco: "7.900",
+    imagem: Imagem2,
+  },
+  {
+    id: 3,
+    pais: "Austrália",
+    preco: "10.900",
+    imagem: Imagem3,
+  },
+  {
+    id: 4,
+    pais: "Estados Unidos",
+    preco: "10.900",
+    imagem: Imagem4,
+  },
+  {
+    id: 5,
+    pais: "Reino Unido",
+    preco: "8.400",
+    imagem: Imagem5,
+  },
+];
 
 function Destinos() {
   return (
-    <section className="destino h-full px-16 md:px-32 py-14">
-      <div className="md:w-[40%]">
+    <section className="destino h-full px-12 lg:px-32 py-14">
+      <div className="max-w-md">
         <p className="text-sm">DESTINOS POPULARES</p>
         <h1 className="text-3xl font-medium">
           Encontre o lugar perfeito para sua próxima jornada
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
-        <DestinoCard />
-        <DestinoCard />
-        <DestinoCard />
-
-        <div className="md:col-span-3 grid md:grid-cols-2 gap-4">
-          <DestinoCard />
-          <DestinoCard />
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-6 gap-4 mt-8">
+        {destinos.map((destino, index) => (
+          <div
+            key={destino.id}
+            className={index < 3 ? "md:col-span-2" : "md:col-span-3"}
+          >
+            <DestinoCard
+              imagem={destino.imagem}
+              pais={destino.pais}
+              preco={destino.preco}
+            />
+          </div>
+        ))}
       </div>
 
-      <p className="text-center underline pt-8">Ver todos os destinos →</p>
+      <a
+        href="#"
+        className="flex justify-center items-center gap-2 text-[#153130] font-medium underline pt-8 duration-300 hover:-translate-y-1"
+      >
+        Ver todos os destinos
+      </a>
     </section>
   );
 }
--- src/components/ui/DestinoCard.jsx
+++ src/components/ui/DestinoCard.jsx
@@ -1,19 +1,22 @@
-import Pais from "../../assets/imagens/toronto.jpg";
+// import Pais from "../../assets/imagens/canada.jpg";
+import { FaArrowRight } from "react-icons/fa6";
 
-function DestinoCard() {
+function DestinoCard({ pais, preco, imagem }) {
   return (
-    <div className="bg-white border border-gray-300 rounded-2xl">
-      <img className="rounded-t-2xl" src={Pais} alt="toronto" />
+    <div className="bg-white border border-gray-300 rounded-2xl transition-all duration-300 hover:-translate-y-2 hover:shadow-xl">
+      <img className="rounded-t-2xl h-full" src={imagem} alt="toronto" />
 
       <div className="py-3 px-5">
-        <h3 className="text-xl font-medium">Canadá</h3>
+        <h3 className="text-xl font-medium">{pais}</h3>
 
         <div className="flex justify-between items-center py-2">
           <div>
             <p className="">A partir de</p>
-            <h3 className="text-lg font-medium">R$ 8.900</h3>
+            <h3 className="text-lg font-medium">R$ {preco}</h3>
           </div>
-          <div className="bg-black text-white rounded-[50%] px-3 py-2">→</div>
+          <div className="bg-black text-white rounded-full p-3 cursor-pointer transition-colors duration-500 hover:bg-[#79a73d]">
+            <FaArrowRight />
+          </div>
         </div>
       </div>
     </div>
